e7ea9ebb8f6f1b3af6d45c688a38971df0d1a5dc
Brian Tan <briantanw@gmail.com>
2020-09-28 19:07:20 -0700

    loaded the data into a sql database

--- main.py
+++ main.py
@@ -80,3 +80,29 @@ if __name__ == "__main__":
     # Validate
     if check_if_valid_data(song_df):
         print("Data valid. Proceed to Load stage")
+    
+    # Load
+    engine = sqlalchemy.create_engine(spotify_api['DATABASE_LOCATION'])
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+    print("Open database successfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Database already exists in the database")
+    
+    conn.close()
+    print("Close database successfully")
